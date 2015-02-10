5b6806498a171b9b18fd61af7a329e0ad9c53267
chr0n1x <heilong24@gmail.com>
2015-02-10 14:39:24 -0500

    CloverCollectorTest

---
+++ tests/Moso/Test/Collectors/CloverCollectorTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Behance\Moso\Tests\Collectors;
+
+use Behance\Moso\Collectors\CloverCollector;
+
+class CloverCollectorTest extends \PHPUnit_Framework_TestCase {
+    protected $clover;
+
+    protected function setUp() {
+        $this->clover = new CloverCollector();
+    }
+
+    /**
+     * @test
+     * @expectedException \LogicException
+     */
+    public function writeMergedResultFailsWithNoFiles() {
+      $this->clover->writeMergedResult('some/file.json');
+    }
+}
